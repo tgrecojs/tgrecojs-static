48986252ee4dbf031c4a0b49541588b48052ad23
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-08-31 00:15:26 -0400

    added CNAME and sheeit + take 2

--- _next/87e5ecde-7664-4239-92ee-f484ea573688/page/post/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/post', function() {
-            var comp = module.exports=webpackJsonp([5],{444:function(e,t,u){e.exports=u(445)},445:function(e,t,u){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(7),r=a(l),n=u(82),d=a(n),o=u(201),f=a(o),s=u(132),c=a(s);u(84);var i=u(130),m=a(i);t.default=(0,m.default)(function(e){var t=e.url.query.post,u=c.default.find(function(e){return e.slug===t});return r.default.createElement("div",null,r.default.createElement(d.default,{href:"/"},r.default.createElement("a",{className:"home-link"},"Home")),r.default.createElement(f.default,{title:u.title,slug:u.slug,date:u.date,author:u.author,body:u.body,tags:u.tags}))},"Post")}},[444]);
-            return { page: comp.default }
-          })
-        
---
+++ _next/8e9eb931-afa6-4085-8762-b8c09664f38f/page/tag/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/tag', function() {
+            var comp = module.exports=webpackJsonp([5],{556:function(e,t,u){e.exports=u(557)},557:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=u(5),n=l(a),r=u(132),d=l(r),f=u(202),o=l(f),c=u(131),s=l(c);u(82);var i=u(128),g=l(i);t.default=(0,g.default)(function(e){var t=e.url.query.tag;return n.default.createElement("div",null,n.default.createElement(d.default,{href:"/"},n.default.createElement("a",null,"Home")),n.default.createElement("h1",null,t),s.default.filter(function(e){return e.tags&&e.tags.includes(t)}).map(function(e){return n.default.createElement(o.default,{key:e.slug,title:e.title,slug:e.slug,date:e.date,author:e.author,body:e.body,tags:e.tags})}))},"Tag")}},[556]);
+            return { page: comp.default }
+          })
+        
