43d38cd9b41ac8ff7172921df5a623b2022630f4
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-09-01 05:02:48 -0400

    Updates

--- _next/b8b27c0e-3591-4278-addf-1a9958ade707/page/post/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/post', function() {
-            var comp = module.exports=webpackJsonp([6],{456:function(e,t,u){e.exports=u(457)},457:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=u(4),r=l(a),n=u(205),o=l(n),d=u(132),f=l(d);u(85);var s=u(84),i=l(s);t.default=(0,i.default)(function(e){var t=e.url.query.post,u=f.default.find(function(e){return e.slug===t});return r.default.createElement("div",null,r.default.createElement(o.default,{title:u.title,slug:u.slug,date:u.date,author:u.author,body:u.body,tags:u.tags}))},"Post")}},[456]);
-            return { page: comp.default }
-          })
-        
