b007473054101ab6c75856f7ea021bcc5b25c8dd
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-09-01 03:55:06 -0400

    Updates

--- _next/65505773-1c54-4e8c-8747-afb5f85ff506/page/contact/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/contact', function() {
-            var comp = module.exports=webpackJsonp([7],{416:function(e,t,l){e.exports=l(417)},417:function(e,t,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=l(4),n=function(e){return e&&e.__esModule?e:{default:e}}(a);t.default=function(){return n.default.createElement("form",{action:"https://formspree.io/thomasjosephgreco@gmail.com",method:"POST"},n.default.createElement("label",{htmlFor:"name"},"Name"),n.default.createElement("input",{type:"text",placeholder:"",name:"name"}),n.default.createElement("label",{htmlFor:"email"},"Email"),n.default.createElement("input",{type:"email",placeholder:"johndoe@gmail.com",name:"_replyto"}),n.default.createElement("input",{type:"submit",value:"Send"}))}}},[416]);
-            return { page: comp.default }
-          })
-        
