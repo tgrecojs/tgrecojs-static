c4f0ce1eaf74269a786cef1038f6be93a265fb64
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-09-06 13:41:59 -0400

    Updates

---
+++ _next/16978228-0e04-47cc-a767-10d676c862b6/page/tag/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/tag', function() {
+            var comp = module.exports=webpackJsonp([6],{573:function(e,t,u){e.exports=u(574)},574:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var r=u(4),l=n(r),a=u(203),f=n(a),c=u(131),o=n(c);u(84);var d=u(83),i=n(d);t.default=(0,i.default)(function(e){var t=e.url.query.tag;return l.default.createElement("div",null,l.default.createElement("h2",{style:{textAlign:"center",color:"white"}},"Current Tag: ",t),o.default.filter(function(e){return e.tags&&e.tags.includes(t)}).map(function(e){return l.default.createElement(f.default,e)}))},"Tag")}},[573]);
+            return { page: comp.default }
+          })
+        
--- _next/e49d14ab-f5a0-4092-9289-3b3d0c9f8416/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([6],{447:function(e,t,u){e.exports=u(448)},448:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(81),a=n(l),f=u(4),r=n(f),d=u(83),o=n(d),c=u(131),i=n(c),s=u(202),p=n(s);t.default=(0,o.default)(function(){return r.default.createElement("div",null,i.default.map(function(e){return r.default.createElement(p.default,(0,a.default)({key:e.slug},e))}))},"Home")}},[447]);
-            return { page: comp.default }
-          })
-        
