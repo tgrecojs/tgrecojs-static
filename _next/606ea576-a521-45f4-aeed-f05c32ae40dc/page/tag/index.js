d47e7895fe8b99ecb77486e90049ad6b25e19e2e
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-09-06 02:36:40 -0400

    Updates

--- _next/606ea576-a521-45f4-aeed-f05c32ae40dc/page/tag/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/tag', function() {
-            var comp = module.exports=webpackJsonp([5],{561:function(e,t,u){e.exports=u(562)},562:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var r=u(4),l=n(r),a=u(202),f=n(a),o=u(131),c=n(o);u(84);var d=u(83),i=n(d);t.default=(0,i.default)(function(e){var t=e.url.query.tag;return l.default.createElement("div",null,l.default.createElement("h2",{style:{textAlign:"center",color:"white"}},t," Posts"),c.default.filter(function(e){return e.tags&&e.tags.includes(t)}).map(function(e){return l.default.createElement(f.default,e)}))},"Tag")}},[561]);
-            return { page: comp.default }
-          })
-        
